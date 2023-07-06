61f989fd0fe1fbba1f877d84cd2f16a903f9265a
unknown <narminna@code.edu.az>
2023-07-06 17:36:12 +0400

    DateTime2 Task2

---
+++ Date2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string date;
+            date=Console.ReadLine();
+            DateTime dt = DateTime.ParseExact(date, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            DateTime dt1 = dt.AddDays(30).AddHours(6).AddMinutes(17);
+            Console.WriteLine(dt1);
+        }
+    }
+}
